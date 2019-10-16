12caac0ad095b35d2497740763934d5fca243be4
mpgadge <mpgadge@gmail.com>
2019-10-16 12:22:40 +0530

    movie-rater-web, first commit on 16th Oct 2019

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import MovieList from './components/movie-list';
+import MovieDetails from './components/movie-details';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //movies = ['titanic', 'avatar','RHTDM'];
+
+  state = {
+    movies: [],
+    selectedMovie: null
+  }
+
+  componentDidMount(){
+    //fetch data from REST API
+    fetch('http://127.0.0.1:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token a7ccc956ea681a615e6928302b55131d76096f44'
+      }
+    }).then( resp => resp.json())    
+    .then( res => this.setState({movies: res}))
+    .catch( error => console.log(error))
+  }
+
+  movieClicked = movie => {
+    this.setState({selectedMovie : movie})
+  }
+
+  render(){     
+            return (
+              <div className="App">      
+                  <h1>Movie Rater Web</h1>   
+                  <div className='layout'>
+                      <MovieList movies={this.state.movies} movieClicked={this.movieClicked}></MovieList> 
+                      <MovieDetails movie={this.state.selectedMovie} />  
+                  </div>
+                  
+              </div>
+            );
+
+  }
+    
+ 
 }
 
 export default App;
