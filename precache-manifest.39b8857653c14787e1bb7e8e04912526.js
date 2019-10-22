5050f024d6417fc449ff8f9b8c1266c42e7f4d70
Gadge Mohanish <Mohanish.Gadge@miba.com>
2019-10-22 18:06:05 +0530

    Updates

---
+++ precache-manifest.39b8857653c14787e1bb7e8e04912526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a5de49c47a8a98a1ec3ef55c5cf455",
+    "url": "/movie-rater-web/index.html"
+  },
+  {
+    "revision": "36cb229e8c1cf2e54649",
+    "url": "/movie-rater-web/static/css/main.1fbf3590.chunk.css"
+  },
+  {
+    "revision": "32bf2159b0e36c79efb4",
+    "url": "/movie-rater-web/static/js/2.ab240e40.chunk.js"
+  },
+  {
+    "revision": "36cb229e8c1cf2e54649",
+    "url": "/movie-rater-web/static/js/main.3a954d04.chunk.js"
+  },
+  {
+    "revision": "c1b52718dc6435aef17d",
+    "url": "/movie-rater-web/static/js/runtime-main.84003c6b.js"
+  }
+]);
