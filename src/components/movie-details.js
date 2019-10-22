a1af38c18838fc90a1f8ff3bbdaaaee8d1d58564
mpgadge <mpgadge@gmail.com>
2019-10-22 18:07:47 +0530

    movie-rater-web, second commit on 22nd oct 2019

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,15 +1,25 @@
 import React,{Component} from 'react';
 
+var Fontawesome = require('react-fontawesome');
+
 class MovieDetails extends Component{
 
     render(){
+        const mov = this.props.movie
         return(
             <React.Fragment>   
             
                 { this.props.movie ? (
                     <div>
                         <h3>{this.props.movie.title}</h3>
-                        <p>{this.props.movie.description}</p>
+
+                        <Fontawesome name='star' className={mov.avg_rating > 0 ? 'orange': 0}/>
+                        <Fontawesome name='star' className={mov.avg_rating > 1 ? 'orange': 0}/>
+                        <Fontawesome name='star' className={mov.avg_rating > 2 ? 'orange': 0}/>
+                        <Fontawesome name='star' className={mov.avg_rating > 3 ? 'orange': 0}/>
+                        <Fontawesome name='star' className={mov.avg_rating > 4 ? 'orange': 0}/>
+                        ({mov.no_of_ratings})
+                        <p>{mov.description}</p>
 
                     </div>
                     
